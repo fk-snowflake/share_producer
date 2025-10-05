36e9dd93115cbd0aecff8d340b0cd0321207c1c0
Fernando Karnagi <fkarnagi@gmail.com>
2025-10-05 19:30:21 +0800

    Remove warehouse

--- 04_share_reader_account.sql
+++ 04_share_reader_account.sql
@@ -6,12 +6,3 @@ CREATE MANAGED ACCOUNT READER_ANALYTICS
 
 -- Add reader account to share
 ALTER SHARE SALES_SHARE ADD ACCOUNTS = zlmnhtd.READER_ANALYTICS;
-
--- Note: Reader accounts don't have compute resources
--- Provider must create virtual warehouse for them
-CREATE WAREHOUSE READER_WH
-    WAREHOUSE_SIZE = 'XSMALL'
-    AUTO_SUSPEND = 60;
-
--- Note: Reader account will specify READER_WH when querying shared data
--- Example: USE WAREHOUSE READER_WH; (executed in reader account session)
