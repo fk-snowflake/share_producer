c6416fed432a751dc2c3c49d2b8b61b4190580dc
Fernando Karnagi <fkarnagi@gmail.com>
2025-10-05 17:16:22 +0800

    Update

---
+++ 04_share_reader_account.sql
@@ -0,0 +1,14 @@
+-- Create managed reader account
+CREATE MANAGED ACCOUNT READER_ANALYTICS
+    ADMIN_NAME = 'reader_admin'
+    ADMIN_PASSWORD = 'SecurePass123!'
+    TYPE = READER;
+
+-- Add reader account to share
+ALTER SHARE SALES_SHARE ADD ACCOUNTS = READER_ANALYTICS;
+
+-- Note: Reader accounts don't have compute resources
+-- Provider must create virtual warehouse for them
+CREATE WAREHOUSE READER_WH
+    WAREHOUSE_SIZE = 'XSMALL'
+    AUTO_SUSPEND = 60;
